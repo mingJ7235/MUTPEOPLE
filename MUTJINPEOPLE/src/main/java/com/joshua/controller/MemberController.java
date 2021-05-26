c46fd5ff291e14368ab390b900f8d4c9862dbf12
김민재 <joshuara7235@gmail.com>
2021-05-27 00:09:04 +0900

    Encrypt and Decypt method made for join and login

--- MUTJINPEOPLE/src/main/java/com/joshua/controller/MemberController.java
+++ MUTJINPEOPLE/src/main/java/com/joshua/controller/MemberController.java
@@ -19,6 +19,8 @@ import lombok.extern.log4j.Log4j;
 public class MemberController {
 	private MemberService service;
 	
+	private static final int KEY = 5;
+	
 	@GetMapping("/home")
 	public void home () {
 		
@@ -32,7 +34,7 @@ public class MemberController {
 	
 	@PostMapping("/join")
 	public String join (MemberVO member, RedirectAttributes rttr) {
-		
+		member.setMemberPw(encrypt(member.getMemberPw()));
 		service.join(member);
 		return "redirect:/member/login";
 	}
@@ -61,6 +63,25 @@ public class MemberController {
 		return result ;
 		
 	}
+	
+	//암호화
+	public String encrypt(String pw) {
+		String en_pw = "";
+		for (int i = 0; i < pw.length(); i++) {
+			en_pw += (char)(pw.charAt(i) * KEY);
+		}
+		return en_pw;
+	}
+		
+		//복호화
+	public String decrypt(String en_pw) {
+		String de_pw = "";
+		for (int i = 0; i < en_pw.length(); i++) {
+			de_pw += (char)(en_pw.charAt(i) / KEY);
+		}
+		return de_pw;
+	}	
+	
 	
 	@GetMapping("/findId")
 	public void findId () {
